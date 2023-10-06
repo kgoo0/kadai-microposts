c6396e2528300aba6ab64f0dd8b509c0ea2a6669
k.m <kgooooog0@gmail.com>
2023-10-06 17:16:17 +0000

    follow, unfollow

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -4,35 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
-
-class UsersController extends Controller
+class UserFollowController extends Controller
 {
-    public function index()
-    {
-        // ユーザ一覧をidの降順で取得
-        $users = User::orderBy('id', 'desc')->paginate(10);
-
-// dd($users);
-        // ユーザ一覧ビューでそれを表示
-        return view('users.index',[
-            'users' => $users,
-        ]);
-    }
-    
-     public function show($id)
-    {
-        // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
-
-        // ユーザ詳細ビューでそれを表示
-        return view('users.show', [
-            'user' => $user,
-        ]);
-    }
-    
     /**
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function store($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
+        \Auth::user()->follow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
+        \Auth::user()->unfollow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    
+     /**
      * ユーザのフォロー一覧ページを表示するアクション。
      *
      * @param  $id  ユーザのid
